172d5f5d7729868d4ca93d465676f0dd7da35ea4
Mayank0017 <mayank02srivastavagmail.com>
2024-02-09 14:05:13 +0530

    socket url

--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -14,7 +14,7 @@ import ScrollableChat from "./ScrollableChat";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; 
+const ENDPOINT = "https://mern-chat-7xvm.onrender.com/"; 
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
