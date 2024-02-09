108c95ab87935d966e368f7660e81dc4a93860f4
Mayank0017 <mayank02srivastavagmail.com>
2024-02-09 13:28:06 +0530

    d

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+const connectDB = async () => {
+    try {
+       const conn= await mongoose.connect("mongodb+srv://SocialMedia:mayank15@cluster1.hfswjhy.mongodb.net/mernChatDB?retryWrites=true&w=majority",{ useNewUrlParser: true ,useUnifiedTopology:true ,});
+        console.log("database successfully connected");
+    } catch (e) {
+        console.log(e)
+        }
+    }
+
+module.exports = connectDB;
